f103bf3fdab53d3c1b12dd4d3d61e7140abbee8d
yuyaebean <yuyaebean@gmail.com>
2019-12-11 18:41:33 +0900

    1.2 Set up the Server like the Pros

--- src/server.js
+++ src/server.js
@@ -1,22 +1,18 @@
 import { GraphQLServer } from 'graphql-yoga';
 import dotenv from 'dotenv';
+import logger from 'morgan';
 
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
+
+// import configurations from dotenv
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-  type Query {
-    hello: String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hi'
-  }
-};
-
 const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.express.use(logger('dev'));
+
 server.start({ port: PORT }, () =>
   console.log(`Server is now running on http://localhost:${PORT}!`)
 );
